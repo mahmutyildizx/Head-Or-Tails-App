7c966af02489d2de04a57e3dbcb4cb0339ca1558
Mahmut <mahmutt.yildiz@outlook.com>
2020-05-21 02:20:16 +0300

    Updates

---
+++ precache-manifest.e1b3d74e241e78ad8fddeb14f67b2d25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b482226b80f77fcf879be89e050f81",
+    "url": "/Head-Or-Tails-App/index.html"
+  },
+  {
+    "revision": "176687c8ab977cf3ea6d",
+    "url": "/Head-Or-Tails-App/static/css/main.b5cb262b.chunk.css"
+  },
+  {
+    "revision": "0ba0ddb80706269d0c2a",
+    "url": "/Head-Or-Tails-App/static/js/2.84df7e86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Head-Or-Tails-App/static/js/2.84df7e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "176687c8ab977cf3ea6d",
+    "url": "/Head-Or-Tails-App/static/js/main.2f7880d3.chunk.js"
+  },
+  {
+    "revision": "030286cc1271b4375a91",
+    "url": "/Head-Or-Tails-App/static/js/runtime-main.5e060e21.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/Head-Or-Tails-App/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/Head-Or-Tails-App/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
